f8184a6056254ebf5212b66a55b5fb27516657f4
Don <don.j.olmstead@gmail.com>
2015-09-06 10:36:50 -0700

    Cleaning up build

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
+set -ex
 
+# Clone dependencies
 git clone https://github.com/dogma-dart/dogma-codegen.git ../dogma-codegen
 git clone https://github.com/dogma-dart/dogma-data.git ../dogma-data
 
+# Get version
 dart --version
 
+# Install dependencies
 pub install
 
+# Run the linter
 pub global activate linter
 pub global run linter .
