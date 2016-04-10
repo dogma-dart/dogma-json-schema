36ba339cec41b4c94d83a2521e5f4931bfbfe57a
Don <don.j.olmstead@gmail.com>
2016-04-10 07:40:01 -0700

    Add metadata library

---
+++ lib/metadata.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+library dogma_json_schema.metadata;
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/metadata/type_metadata.dart';
