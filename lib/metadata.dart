0c9feca01f52be3d6bc978262831f36a823472cb
Don <don.j.olmstead@gmail.com>
2016-04-11 19:14:19 -0700

    Expose class generation

--- lib/metadata.dart
+++ lib/metadata.dart
@@ -10,5 +10,6 @@ library dogma_json_schema.metadata;
 //---------------------------------------------------------------------
 
 export 'src/metadata/commented.dart';
+export 'src/metadata/class_metadata.dart';
 export 'src/metadata/enum_metadata.dart';
 export 'src/metadata/type_metadata.dart';
