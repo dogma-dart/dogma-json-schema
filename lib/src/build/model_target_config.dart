f9d0361f3fa547a8f7cb403892f6cdc913dc2507
Don <don.j.olmstead@gmail.com>
2016-06-28 23:23:22 -0700

    Create a config for models

---
+++ lib/src/build/model_target_config.dart
@@ -0,0 +1,19 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_codegen/build.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Configuration for a target within the ModelBuilder.
+class ModelTargetConfig extends TargetConfig {
+
+}
