16dabc10709f38b900e3cbab2681b9eb3bd8111a
Don <don.j.olmstead@gmail.com>
2016-06-28 23:23:39 -0700

    Create the build library

---
+++ lib/build.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+library dogma_json_schema.build;
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/build/model_builder.dart';
+export 'src/build/model_target_config.dart';
+export 'src/build/schema_metadata_step.dart';
